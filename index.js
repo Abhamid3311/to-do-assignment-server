86ccb3625fab39e71d4d2c07d4ab18a27aa7b87d
Hamid <www.abdulhamid3311@gmail.com>
2022-07-25 14:55:15 +0600

    addded delete func

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,7 +11,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0oc3y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 
 
 
@@ -34,22 +33,25 @@ async function run() {
             const todos = await cursor.toArray();
             res.send(todos);
         });
+
+        //DELETE
+        app.delete('/todo/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const deleteItem = await todosCollections.deleteOne(query);
+            res.send(deleteItem);
+        });
+
     }
     finally {
 
     }
 };
 
-
-
-
-
-
-
-
 run().catch(console.dir);
 
 
+//Testing API
 app.get('/', (req, res) => {
     res.send('Hello From To-do app')
 });
